45e93e773f1a93d4f92cab1fb5b88fb026c71689
waelsk <36414767+waelsk@users.noreply.github.com>
2020-04-04 14:40:28 +0100

    Update Jenkinsfile.groovy

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,39 +1,84 @@
 pipeline {
-    agent any
+agent {
+label "master"
+}
+tools {
+// Note: this should match with the tool name configured in your jenkins instance (JENKINS_URL/configureTools/)
+maven "maven"
+}
+environment {
+// This can be nexus3 or nexus2
+NEXUS_VERSION = "nexus3"
+// This can be http or https
+NEXUS_PROTOCOL = "http"
+// Where your Nexus is running
+NEXUS_URL = "localhost:8081"
+// Repository where we will upload the artifact
+NEXUS_REPOSITORY = "test_repository"
+// Jenkins credential id to authenticate to Nexus OSS
+NEXUS_CREDENTIAL_ID = "nexus-credentials"
+}
+stages {
+stage("clone code") {
+steps {
+script {
+// Let's clone the source
+git 'https://github.com/aymenkazdaghli/cirepository.git';
+}
+}
+}
+stage("mvn build") {
+steps {
+script {
+// If you are using Windows then you should use "bat" step
+// Since unit testing is out of the scope we skip them
+bat "mvn package -DskipTests=true"
+}
+}
+}
+stage("publish to nexus") {
+steps {
+script {
+// Read POM xml file using 'readMavenPom' step , this step 'readMavenPom' is included in: https://plugins.jenkins.io/pipeline-utility-steps
+pom = readMavenPom file: "pom.xml";
+// Find built artifact under target folder
+filesByGlob = findFiles(glob: "target/*.${pom.packaging}");
+// Print some info from the artifact found
+echo "${filesByGlob[0].name} ${filesByGlob[0].path} ${filesByGlob[0].directory} ${filesByGlob[0].length} ${filesByGlob[0].lastModified}"
+// Extract the path from the File found
+artifactPath = filesByGlob[0].path;
+// Assign to a boolean response verifying If the artifact name exists
+artifactExists = fileExists artifactPath;
+if(artifactExists) {
+echo "*** File: ${artifactPath}, group: ${pom.groupId}, packaging: ${pom.packaging}, version ${pom.version}";
+nexusArtifactUploader(
+nexusVersion: NEXUS_VERSION,
+protocol: NEXUS_PROTOCOL,
+nexusUrl: NEXUS_URL,
+groupId: pom.groupId,
+version: pom.version,
+repository: NEXUS_REPOSITORY,
+credentialsId: NEXUS_CREDENTIAL_ID,
+artifacts: [
+// Artifact generated such as .jar, .ear and .war files.
+[artifactId: pom.artifactId,
+classifier: '',
+file: artifactPath,
+type: pom.packaging],
+// Lets upload the pom.xml file for additional information for Transitive dependencies
+[artifactId: pom.artifactId,
+classifier: '',
+file: "pom.xml",
+type: "pom"]
+]
+);
+} else {
+error "*** File: ${artifactPath}, could not be found";
+}
+}
+}
 
-    stages {
- stage('clone code') {
-            steps {
-              git 'https://github.com/waelsk/Maven_Demo.git'
-                echo 'Clone done..'
-            }
-        }
-        stage('Build') {
-            steps {
-              bat label: '', script: 'mvn clean install'
-                echo 'Building..'
-            }
-        }
-         
- 
-         stage('Test') {
-            steps {
-               bat label: '', script: 'mvn test'
-                echo 'Testing..'
-            }
-        }
-        
-        stage('Deploy') {
-            steps {
-                echo 'Deploying....'
-            }
-        }
-      
-    }
-      post('send email') {
-            always {
-              emailext body: 'test', subject: 'C1', to: 'wael.soukeh@esprit.tn'
-               echo 'success..'
-            }
-        }
+}
+
+}
 }
